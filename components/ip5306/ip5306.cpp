524628ebcd8249847e577020f1c24718c9a726fa
Hendrik L <hendrik+dev@xd0.de>
2026-07-19 21:09:58 +0200

    refactor registers

--- components/ip5306/ip5306.cpp
+++ components/ip5306/ip5306.cpp
@@ -7,107 +7,94 @@ namespace ip5306 {
 
 static const char *TAG = "ip5306";
 
-static const uint8_t IP5306_REG_SYS_CTL0 = 0x00;
-static const uint8_t IP5306_REG_SYS_CTL1 = 0x01;
-//static const uint8_t IP5306_REG_SYS_CTL2 = 0x02;
-//static const uint8_t IP5306_REG_CHG_CTL0 = 0x20;
-//static const uint8_t IP5306_REG_CHG_CTL1 = 0x21;
-static const uint8_t IP5306_REG_CHG_DIG  = 0x24;
-
-static const uint8_t IP5306_REG_READ0    = 0x70;  // charge en
-static const uint8_t IP5306_REG_READ1    = 0x71;  // charge full
-static const uint8_t IP5306_REG_READ2    = 0x72;  // load
-//static const uint8_t IP5306_REG_READ3    = 0x77;
-static const uint8_t IP5306_REG_LEVEL    = 0x78;  // bat level
-
-// REG_SYS_CTL0
-static const uint8_t BOOST_ENABLE_BIT    = 0x20;
-static const uint8_t CHARGE_OUT_BIT      = 0x10;
-static const uint8_t BOOT_ON_LOAD_BIT    = 0x04;
-static const uint8_t BOOST_OUT_BIT       = 0x02;
-static const uint8_t BOOST_BUTTON_EN_BIT = 0x01;
-
-// REG_SYS_CTL1
-//static const uint8_t BOOST_SET_BIT   = 0x80;
-static const uint8_t WLED_SET_BIT      = 0x40;
-static const uint8_t SHORT_BOOST_BIT   = 0x20;
-static const uint8_t VIN_ENABLE_BIT    = 0x04;
-static const uint8_t LOW_POWER_SHUTDOWN_BIT  = 0x01;
-
-// REG_READ0
-static const uint8_t CHARGE_ENABLE_BIT = 0x08;
-
-// REG_READ1
-static const uint8_t CHARGE_FULL_BIT   = 0x08;
-
-// REG_READ2
-static const uint8_t HEAVY_LOAD_BIT   = 0x04;
-
-
 float IP5306::get_setup_priority() const { return setup_priority::IO; }
 
 void IP5306::setup() {
   ESP_LOGD(TAG, "Setting up ip5306...");
-  uint8_t data;
-  if (this->read_register(IP5306_REG_SYS_CTL0, &data, 1) != i2c::ERROR_OK) {
+  if (this->read_register(static_cast<uint8_t>(RegisterMap::SYS_CTL0), &this->status_.reg_sys_ctl0.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "setup failed");
     this->mark_failed();
     return;
   }
-  data = data | BOOST_ENABLE_BIT | CHARGE_OUT_BIT | BOOT_ON_LOAD_BIT | BOOST_OUT_BIT | BOOST_BUTTON_EN_BIT;
-  if (this->write_register(IP5306_REG_SYS_CTL0, &data, 1) != i2c::ERROR_OK) {
+  this->status_.reg_sys_ctl0.boost_enable = true;
+  this->status_.reg_sys_ctl0.charger_enable = true;
+  this->status_.reg_sys_ctl0.power_on_load = true;
+  this->status_.reg_sys_ctl0.boost_output_enable = true;
+  this->status_.reg_sys_ctl0.button_shutdown = true;
+  if (this->write_register(static_cast<uint8_t>(RegisterMap::SYS_CTL0), &this->status_.reg_sys_ctl0.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "setup failed");
     this->mark_failed();
     return;
   }
 
-  if (this->read_register(IP5306_REG_SYS_CTL1, &data, 1) != i2c::ERROR_OK) {
+  if (this->read_register(static_cast<uint8_t>(RegisterMap::SYS_CTL1), &this->status_.reg_sys_ctl1.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "setup failed");
     this->mark_failed();
     return;
   }
-  data = data | VIN_ENABLE_BIT;
-  if (this->write_register(IP5306_REG_SYS_CTL1, &data, 1) != i2c::ERROR_OK) {
+  this->status_.reg_sys_ctl1.boost_after_vin_unplug = true;
+  if (this->write_register(static_cast<uint8_t>(RegisterMap::SYS_CTL1), &this->status_.reg_sys_ctl1.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "setup failed");
     this->mark_failed();
     return;
   }
+
+  set_battery_voltage(BatteryVoltage::BAT_VOLTAGE_4_2);
+  set_voltage_pressure(VoltagePressure::PRESSURIZED_28);
+  set_light_load_shutdown_time(ShutdownTime::SHUTDOWN_64s);
 }
 
 void IP5306::update() {
   // Work to be done at each update interval
-  uint8_t data;
   if (this->battery_level_ != nullptr) {
-    if (this->read_register(IP5306_REG_LEVEL, &data, 1) != i2c::ERROR_OK) {
+    if (this->read_register(static_cast<uint8_t>(RegisterMap::LEVEL), &this->status_.reg_level.raw, 1) != i2c::ERROR_OK) {
       ESP_LOGW(TAG, "unable to read level");
       this->status_set_warning();
       return;
     }
     float value = 0;
-    switch (data & 0xF0) {
-      case 0xE0: value = 25; break;
-      case 0xC0: value = 50; break;
-      case 0x80: value = 75; break;
-      case 0x00: value = 100; break;
+    switch (this->status_.reg_level.charge_level) {
+      case ChargeLevel::LEVEL_25:  value = 25; break;
+      case ChargeLevel::LEVEL_50:  value = 50; break;
+      case ChargeLevel::LEVEL_75:  value = 75; break;
+      case ChargeLevel::LEVEL_100: value = 100; break;
       //default: value = 0; break;
     }
     if (this->battery_level_ != nullptr)
       this->battery_level_->publish_state(value);
   }
-  if (this->read_register(IP5306_REG_READ0, &data, 1) != i2c::ERROR_OK) {
+  if (this->read_register(static_cast<uint8_t>(RegisterMap::READ0), &this->status_.reg_read0.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGW(TAG, "unable to read status");
     this->status_set_warning();
     return;
   }
   if (this->charger_connected_ != nullptr)
-    this->charger_connected_->publish_state(data & CHARGE_ENABLE_BIT);
-  if (this->read_register(IP5306_REG_READ1, &data, 1) != i2c::ERROR_OK) {
+    this->charger_connected_->publish_state(this->status_.reg_read0.charge_enable);
+  if (this->read_register(static_cast<uint8_t>(RegisterMap::READ1), &this->status_.reg_read1.raw, 1) != i2c::ERROR_OK) {
     ESP_LOGW(TAG, "unable to read status");
     this->status_set_warning();
     return;
   }
   if (this->charge_full_ != nullptr)
-    this->charge_full_->publish_state(data & CHARGE_FULL_BIT);
+    this->charge_full_->publish_state(this->status_.reg_read1.battery_full);
+}
+
+void IP5306::set_light_load_shutdown_time(ShutdownTime time) {
+  this->read_register(static_cast<uint8_t>(RegisterMap::SYS_CTL2), &this->status_.reg_sys_ctl2.raw, 1);
+  this->status_.reg_sys_ctl2.light_load_shutdown_time = time;
+  this->write_register(static_cast<uint8_t>(RegisterMap::SYS_CTL2), &this->status_.reg_sys_ctl2.raw, 1);
+}
+
+void IP5306::set_battery_voltage(BatteryVoltage voltage) {
+  this->read_register(static_cast<uint8_t>(RegisterMap::CHG_CTL2), &this->status_.reg_chg_ctl2.raw, 1);
+  this->status_.reg_chg_ctl2.battery_voltage = voltage;
+  this->write_register(static_cast<uint8_t>(RegisterMap::CHG_CTL2), &this->status_.reg_chg_ctl2.raw, 1);
+}
+
+void IP5306::set_voltage_pressure(VoltagePressure pressure) {
+  this->read_register(static_cast<uint8_t>(RegisterMap::CHG_CTL2), &this->status_.reg_chg_ctl2.raw, 1);
+  this->status_.reg_chg_ctl2.voltage_pressure = pressure;
+  this->write_register(static_cast<uint8_t>(RegisterMap::CHG_CTL2), &this->status_.reg_chg_ctl2.raw, 1);
 }
 
 }  // namespace ip5306
--- components/ip5306/ip5306.h
+++ components/ip5306/ip5306.h
@@ -8,6 +8,207 @@
 namespace esphome {
 namespace ip5306 {
 
+
+enum class RegisterMap : uint8_t {
+  SYS_CTL0 = 0x00,
+  SYS_CTL1 = 0x01,
+  SYS_CTL2 = 0x02,
+  CHG_CTL0 = 0x20,
+  CHG_CTL1 = 0x21,
+  CHG_CTL2 = 0x22,
+  CHG_CTL3 = 0x23,
+  CHG_DIG  = 0x24,
+  READ0    = 0x70,  // charge en
+  READ1    = 0x71,  // charge full
+  READ2    = 0x72,  // load
+  READ3    = 0x77,  // buttons
+  LEVEL    = 0x78,  // bat level
+};
+
+union RegSysCtl0 {
+  struct {
+    bool button_shutdown : 1;     // 0 rw
+    bool boost_output_enable : 1; // 1 rw
+    bool power_on_load : 1;       // 1 rw
+    bool reserved_3 : 1;          // 1
+    bool charger_enable : 1;      // 1 rw
+    bool boost_enable : 1;        // 1 rw
+    bool reserved_6 : 1;          // 0
+    bool reserved_7 : 1;          // 1
+  };
+  uint8_t raw;
+};
+
+union RegSysCtl1 {
+  struct {
+    bool low_battery_shutdown : 1;             // 1 rw
+    bool reserved_1 : 1;                       // 0
+    bool boost_after_vin_unplug : 1;           // 1 rw 
+    bool reserved_3 : 1;
+    bool reserved_4 : 1;
+    bool short_press_boost_switch : 1;         // 0 rw
+    bool flashlight_ctrl_signal_selection : 1; // 0 rw
+    bool boost_ctrl_signal_selection : 1;      // 0 rw
+  };
+  uint8_t raw;
+};
+
+enum class ShutdownTime : uint8_t {
+  SHUTDOWN_8s  = 0b00,
+  SHUTDOWN_32s = 0b01,
+  SHUTDOWN_16s = 0b10,
+  SHUTDOWN_64s = 0b11,
+};
+
+union RegSysCtl2 {
+  struct {
+    uint8_t reserved_0_1 : 2;             // 00
+    ShutdownTime light_load_shutdown_time : 2; // 00 rw
+    bool long_press_time : 1;             // 0 rw
+    uint8_t reserved_5_7 : 3;
+  };
+  uint8_t raw;
+};
+
+enum class ChargeStopVoltage : uint8_t {
+  CUT_OFF_VOLTAGE_0  = 0b00,     // 4.14/4.26/4.305/4.35  V
+  CUT_OFF_VOLTAGE_1  = 0b01,     // 4.17/4.275/4.32/4.365 V
+  CUT_OFF_VOLTAGE_2  = 0b10,     // 4.185/4.29/4.335/4.38 V
+  CUT_OFF_VOLTAGE_3  = 0b11,     // 4.2/4.305/4.35/4.395  V
+};
+
+union RegChgCtl0 {
+  struct {
+    ChargeStopVoltage charging_full_stop_voltage : 2; // 10 rw
+    uint8_t reserved_2_7 : 6;
+  };
+  uint8_t raw;
+};
+
+enum class StopCurrent : uint8_t {
+  CURRENT_200  = 0,
+  CURRENT_400  = 1,
+  CURRENT_500  = 2,
+  CURRENT_600  = 3,
+};
+
+enum class VoltageSetting : uint8_t {
+  VOUT_4_45 = 0,
+  VOUT_4_5  = 1,
+  VOUT_4_55 = 2,
+  VOUT_4_6  = 3,
+  VOUT_4_65 = 4,
+  VOUT_4_7  = 5,
+  VOUT_4_75 = 6,
+  VOUT_4_8  = 7,
+};
+
+union RegChgCtl1 {
+  struct {
+    uint8_t reserved_0_1 : 2;
+    VoltageSetting charge_under_voltage_loop : 3; // 101 rw
+    uint8_t reserved_5 : 1;
+    StopCurrent end_charge_current_detection : 2; // 01 rw
+  };
+  uint8_t raw;
+};
+
+enum class BatteryVoltage : uint8_t {
+  BAT_VOLTAGE_4_4  = 3,
+  BAT_VOLTAGE_4_35 = 2,
+  BAT_VOLTAGE_4_3  = 1,
+  BAT_VOLTAGE_4_2  = 0,
+};
+
+enum class VoltagePressure : uint8_t {
+  PRESSURIZED_42  = 3,
+  PRESSURIZED_28  = 2,
+  PRESSURIZED_14  = 1,
+  PRESSURIZED_0   = 0,
+};
+
+union RegChgCtl2 {
+  struct {
+    VoltagePressure voltage_pressure : 2; // 01 rw
+    BatteryVoltage battery_voltage : 2;  // 00 rw
+    uint8_t reserved_4_7 : 4;     // 0000
+  };
+  uint8_t raw;
+};
+
+union RegChgCtl3 {
+  struct {
+    uint8_t reserved_0_4 : 5;
+    bool charging_constant_current_loop_selection : 1; // 1 rw 
+    uint8_t reserved_6_7 : 2;
+  };
+  uint8_t raw;
+};
+
+union RegChgDig {
+  struct {
+    uint8_t charger_vin_end_current : 5; // rw 
+    uint8_t reserved_5_7 : 3;
+  };
+  uint8_t raw;
+};
+
+union RegRead0 {
+  struct {
+    bool reserved_0_2 : 3;
+    bool charge_enable : 1;
+    bool reserved_4_7 : 4;
+  };
+  uint8_t raw;
+};
+
+union RegRead1 {
+  struct {
+    bool reserved_0_2 : 3; // r
+    bool battery_full : 1; // r
+    bool reserved_4_7 : 4; // r
+  };
+  uint8_t raw;
+};
+
+union RegRead2 {
+  struct {
+    bool reserved_0_1 : 2; // r
+    bool load_level : 1;   // r
+    bool reserved_3_7 : 5; // r
+  };
+  uint8_t raw;
+};
+
+union RegRead3 {
+  struct {
+    bool short_press_detect : 1;  // 0 rw
+    bool long_press_detect : 1;   // 0 rw
+    bool double_click_detect : 1; // 0 rw
+    bool reserved_3_7 : 5;
+  };
+  uint8_t raw;
+};
+
+enum class ChargeLevel : uint8_t {
+  LEVEL_0   = 0b1111,
+  LEVEL_25  = 0b1110,
+  LEVEL_50  = 0b1100,
+  LEVEL_75  = 0b1000,
+  LEVEL_100 = 0b0000,
+};
+
+union RegLevel {
+  struct {
+    uint8_t reserved_0_3 : 4;
+    ChargeLevel charge_level : 4;
+  };
+  uint8_t raw;
+};
+
+
+
+
 class IP5306 : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
   void setup() override;
@@ -18,11 +219,25 @@ class IP5306 : public sensor::Sensor, public PollingComponent, public i2c::I2CDe
   void set_battery_level(sensor::Sensor *sensor) { this->battery_level_ = sensor; }
   void set_charger_connected(binary_sensor::BinarySensor *sensor) { this->charger_connected_ = sensor; }
   void set_charge_full(binary_sensor::BinarySensor *sensor) { this->charge_full_ = sensor; }
+  
+  void set_battery_voltage(BatteryVoltage voltage);
+  void set_voltage_pressure(VoltagePressure pressure);
+
+  void set_light_load_shutdown_time(ShutdownTime time);
 
  protected:
   sensor::Sensor *battery_level_{nullptr};
   binary_sensor::BinarySensor *charger_connected_{nullptr};
   binary_sensor::BinarySensor *charge_full_{nullptr};
+  struct {
+    RegSysCtl0 reg_sys_ctl0;
+    RegSysCtl1 reg_sys_ctl1;
+    RegSysCtl2 reg_sys_ctl2;
+    RegChgCtl2 reg_chg_ctl2;
+    RegRead0 reg_read0;
+    RegRead1 reg_read1;
+    RegLevel reg_level;
+  } status_{};
 };
 
 }  // namespace ip5306
